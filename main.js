22270a1f871a5190f8f55cb80b58a5b8c3056962
zenovian99 <85941559+zenovian99@users.noreply.github.com>
2022-08-09 19:24:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d")
+
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("aplhakey", my_aplhakey );
+
+function my_aplhakey(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML= " you pressed aplhabet key";
+console.log("aplhabet key")
+		}
+
+		
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+window.addEventListener("numkey", my_numkey );
+function my_numkey(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=48 && keyPressed<=57))
+		{
+			numberkey();
+			document.getElementById("d1").innerHTML= " you pressed number key";
+console.log("number key")
+		}
+
+		
+	
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png";
+	add();
+}
+function numberkey()
+{
+	img_image="numkey.png";
+	add()
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add()
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add()
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
